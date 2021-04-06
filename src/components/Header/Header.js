5fa3203261c970dabbaa9e11d21363cc3cf0f524
danielclubb <danieljohnclubb@gmail.com>
2021-04-06 22:33:14 +0100

    updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import './Header.css';
-import Link from '../Link/Link';
-const Header = ({ logo }) => (
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Edit <code>src/App.js</code> and save to reload DAN.
-        </p>
-        <Link url="https://reactjs.org" title="Learn React" />
-  </header>
+import styled from 'styled-components';
+import logo from '../../logo.svg';
+
+const HeaderWrapper = styled.div`
+    background-color: #282c34;
+    height: 100%; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+const Logo = styled.img`
+    height: 64px;
+    pointer-events: none;
+`;
+
+const Header = () => (
+    <HeaderWrapper>
+        <Logo src={logo} alt='logo' />        
+        <h1>
+            My Github Portfolio
+        </h1>
+    </HeaderWrapper>
 );
 
 export default Header;
